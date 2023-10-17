3c226662fa07afa3117b9a6c5ee87358e5bd6434
Christian Bongard <christian.bongard90@gmail.com>
2023-10-18 00:18:34 +0400

    refactor car insertion

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
-console.log("Connected")
+console.log("Connected");
 
 // step 1 fetch all cars
 const garageName = "garage-1385";
 const garageURL = `https://wagon-garage-api.herokuapp.com/${garageName}/cars`;
-const carList = document.querySelector('.cars-list');
-// define the function
-const fetchAllCars = () => {
-  // we call the server
-  fetch(garageURL)
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data); // Array
-      carList.innerHTML = "";
-      // We iterate on the array
-      data.forEach((car) => {
-        // we create a card for EACH cars
-        const carCard = `
+const carList = document.querySelector(".cars-list");
+
+// Create a function to create the card
+const insertCarCard = (car) => {
+  const carCard = `
           <div class="car">
             <div class="car-image">
               <img src="http://loremflickr.com/280/280/Ferrari 308 GTS" />
@@ -27,11 +19,25 @@ const fetchAllCars = () => {
             </div>
           </div>
         `;
-        // Insert into the list
-        carList.insertAdjacentHTML('afterbegin', carCard);
-      })
-    })
-}
+  // Insert into the list
+  carList.insertAdjacentHTML("afterbegin", carCard);
+};
+
+// define the function
+const fetchAllCars = () => {
+  // we call the server
+  fetch(garageURL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data); // Array
+      carList.innerHTML = "";
+      // We iterate on the array
+      data.forEach((car) => {
+        // we create a card for EACH cars
+        insertCarCard(car);
+      });
+    });
+};
 
 // call the function
 fetchAllCars();
@@ -41,23 +47,23 @@ const submitButton = document.querySelector(".car-form");
 
 console.log(submitButton);
 // Listen to the submit event
-submitButton.addEventListener('submit', (event) => {
+submitButton.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log('click click Im here');
+  console.log("click click Im here");
   // we get the information from the inputs
   console.log(event.currentTarget);
   const formData = new FormData(event.currentTarget);
   console.log(formData);
-  const newCar = Object.fromEntries(formData)
-  console.log('car object', newCar);
+  const newCar = Object.fromEntries(formData);
+  console.log("car object", newCar);
   // we post the new car to the data
   fetch(garageURL, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(newCar)
-  })
-    .then(() => fetchAllCars())
-    event.currentTarget.reset();
-})
+    body: JSON.stringify(newCar),
+  }).then(() => fetchAllCars());
+  // we reset the form
+  event.currentTarget.reset();
+});
